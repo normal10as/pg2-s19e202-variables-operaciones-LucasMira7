4975dc04d15e9e087b56b5b1625c8cf7eb648113
Lucas Mira <lucasgastonmira@outlook.com.ar>
2019-05-22 17:41:02 -0300

    1.2 terminado

--- variables/Variables_Tipos/variables_tipos.vb
+++ variables/Variables_Tipos/variables_tipos.vb
@@ -4,14 +4,14 @@
         Dim NombrePersona, LugarNacimiento, Nacionalidad, NombreCalle As String
         Dim DNI, NroHabitantesNacionales As UInteger
         Dim FechaNacimiento As Date
-        Dim TemperaturaAmbiente, DistanciaEntre2Ciudades, ProfundidadMar, PesoProducto, PrecioProducto, AlturaCorporal As Single
-        Dim AlturaCalle As UShort
+        Dim TemperaturaAmbiente, DistanciaEntre2Ciudades, ProfundidadMar, PesoProducto, PrecioProducto As Single
+        Dim AlturaCalle, AlturaCorporal As UShort
 
         NombrePersona = "Lucas"
         DNI = 43096189
         LugarNacimiento = "Corrientes"
         FechaNacimiento = "8-12-99"
-        AlturaCorporal = 2.7
+        AlturaCorporal = 270
         Nacionalidad = "Argentino"
         TemperaturaAmbiente = 12
         NombreCalle = "Bolivar"
@@ -22,20 +22,20 @@
         PesoProducto = 60.4
         PrecioProducto = 15.5
 
-        Console.WriteLine("Nombre de una Persona: " + NombrePersona)
-        Console.WriteLine("Número de Documento: " + DNI)
-        Console.WriteLine("Lugar de Nacimiento: " + LugarNacimiento)
-        Console.WriteLine("Fecha de Nacimiento: " + FechaNacimiento)
-        Console.WriteLine("Altura Corporal: " + AlturaCorporal + "cms.")
-        Console.WriteLine("Nacionalidad: " + Nacionalidad)
-        Console.WriteLine("Temperatura Ambiente: " + TemperaturaAmbiente)
-        Console.WriteLine("Nombre de la Calle: " + NombreCalle)
-        Console.WriteLine("Altura de la Calle: " + AlturaCalle)
-        Console.WriteLine("Distancia entre 2 Ciudades Cualquiera: " + DistanciaEntre2Ciudades + "kms.")
-        Console.WriteLine("Profundidad del Mar: " + ProfundidadMar + "m.")
-        Console.WriteLine("Cantidad de Habitantes del Territorio Nacional: " + NroHabitantesNacionales)
-        Console.WriteLine("Peso de un Producto de Supermercado: " + PesoProducto + "kg.")
-        Console.WriteLine("Monto de un Artículo de Supermercado: " + PrecioProducto)
+        Console.WriteLine("Nombre de una Persona: " & NombrePersona)
+        Console.WriteLine("Número de Documento: " & DNI)
+        Console.WriteLine("Lugar de Nacimiento: " & LugarNacimiento)
+        Console.WriteLine("Fecha de Nacimiento: " & FechaNacimiento)
+        Console.WriteLine("Altura Corporal: " & AlturaCorporal & "cms")
+        Console.WriteLine("Nacionalidad: " & Nacionalidad)
+        Console.WriteLine("Temperatura Ambiente: " & TemperaturaAmbiente & "°")
+        Console.WriteLine("Nombre de la Calle: " & NombreCalle)
+        Console.WriteLine("Altura de la Calle: " & AlturaCalle)
+        Console.WriteLine("Distancia entre 2 Ciudades Cualquiera: " & DistanciaEntre2Ciudades & "kms")
+        Console.WriteLine("Profundidad del Mar: " & ProfundidadMar & "ms")
+        Console.WriteLine("Cantidad de Habitantes del Territorio Nacional: " & NroHabitantesNacionales)
+        Console.WriteLine("Peso de un Producto de Supermercado: " & PesoProducto & "kg")
+        Console.WriteLine("Monto de un Artículo de Supermercado: " & PrecioProducto)
     End Sub
 
 End Module
