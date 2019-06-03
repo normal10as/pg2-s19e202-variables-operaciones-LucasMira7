0c007acdcffd02d148782aad5e1f5711e1c3fbc6
Lucas Mira <lucasgastonmira@gmail.com.ar>
2019-06-03 18:48:24 -0300

    modificaciones

---
+++ variables/Concatenación/Concatenación.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Concatenación
+    Sub Main(args As String())
+        Dim Empresa, Calle As String
+        Dim AlturaCalle As UShort
+        Dim FechaInicio As Date
+    End Sub
+End Module
